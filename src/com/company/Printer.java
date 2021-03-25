9b5fb9b75e4e41217def7e5b39e7ced17d2bcf75
unknown <jupobatero1@gmail.com>
2021-03-25 13:05:10 -0300

    Genero las clases impresora y tares

---
+++ src/com/company/Printer.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Printer {
+    private int count =0;
+    private boolean working = false;
+
+    public synchronized void print(Integer pages){
+        try{
+            while(working){
+                wait();
+            }
+            working =true;
+            System.out.println("Comienzo de la impresion-> "+count);
+            Thread.sleep(3000);
+            for (int i =0; i<pages;i++){
+                Thread.sleep(2500);
+                System.out.println("Imprimiedo pagina numero: "i+1);
+            }
+
+            System.out.println("Fin de la impresion-> "+count);
+            Thread.sleep(3000);
+            clean();
+            working = false;
+            notifyAll();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void clean()
+    {
+        for (int i=0; i < 60; i++)
+        {
+            System.out.println("");
+        }
+    }
+
+}
---
+++ src/com/company/Task.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Task extends Thread{
+    private Integer pages;
+    private Printer printer;
+
+    public Task(Integer pages, Printer printer) {
+        this.pages = pages;
+        this.printer = printer;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public Printer getPrinter() {
+        return printer;
+    }
+
+    public void setPrinter(Printer printer) {
+        this.printer = printer;
+    }
+
+    public void run(){
+        try{
+            printer.print(this.pages);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
